500b9338ca2351da7319a7e6418fa2c154deb4c9
Patrick Billingsley <prbillingsley89@gmail.com>
2021-11-13 17:42:20 -0600

    Change database form SQLite3 to PostgreSQL to work with Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_13_143909) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
