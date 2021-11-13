0601f816e7cfe2f0ce41b338496f2ed146d937a8
Patrick Billingsley <prbillingsley89@gmail.com>
2021-11-12 22:35:12 -0600

    Part 7: Adding Bootstrap CSS & Javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # GET /about
   get "about", to: "about#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to:"main#index"
 end
